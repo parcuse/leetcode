25267e8dbbfdb2c59e03c7a8405aa1dbcedc7f56
parcuse <kebiao.deng@ele.me>
2019-01-07 16:14:51 +0800

    LongestStr

--- LongestStr.cpp
+++ LongestStr.cpp
@@ -4,18 +4,15 @@
         size_t max = 0;
         size_t length = s.size();
         for (int cursor2 = 0;cursor2 < length; cursor2++){
-            cout<<cursor2<<s[cursor2]<<endl;
             if (max_data.find(s[cursor2]) == string::npos){
-                string tmp_max_data = s.substr(cursor1, cursor2+1);
-                max_data = tmp_max_data.size()>max?tmp_max_data:max_data;
-                max = tmp_max_data.size()>max?tmp_max_data.size():max;
-                cout<<max_data<<"============="<<endl;
+                string tmp_max_data = s.substr(cursor1, cursor2 + 1 - cursor1);
+                max_data = tmp_max_data.size() > max?tmp_max_data:max_data;
+                max = tmp_max_data.size() > max?tmp_max_data.size():max;
             }
             else{
-                cursor1 = (int) max_data.find(s[cursor2]) + 1 ;
+                cursor1 = (int) s.substr(cursor1).find(s[cursor2]) + 1 + cursor1;
             }
         }
-        cout<<max_data<<endl;
         return (int) max;
     }
 
